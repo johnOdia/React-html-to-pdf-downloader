defde3e8d8d283d618a1ddb18c024e49aecef269
johnOdia <63448302+johnOdia@users.noreply.github.com>
2021-10-15 19:19:02 +0100

    initial commit

---
+++ src/components/PdfDownloader.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import jsPDF from 'jspdf';
+import TableRowPdfData from './TableRowPdfData';
+import "./PdfDownloader.css"
+
+const PdfDownloader = () => {
+    const [value, setValue] = useState([]);
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [isTableVisible, setTableVisible] = useState(false);
+
+    const generatePdf = () => {
+        const doc = new jsPDF("p", "pt", "a4");
+        const html = document.querySelector('#content')
+        doc.html(html, {
+            callback: (pdf) => {
+                pdf.save("mypdf.pdf");
+            }
+        })
+        setTimeout(() => {
+            setValue([]);
+            setTableVisible(false);
+        }, 1000)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        generatePdf();
+    }
+    const handleChange = (e) => {
+        const data = e.target.value;
+        e.target.placeholder === 'Name' ? setName(data) : setAge(data);
+    }
+    const handleClick = () => {
+        const entry = { name, age };
+        setValue([
+            ...value,
+            entry,
+        ]);
+        setAge('');
+        setName('');
+    }
+    const showTable = () => {
+        isTableVisible ? setTableVisible(false) : setTableVisible(true);
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <form onSubmit={handleSubmit} className='form'>
+                    <div className='inputContainer'>
+                        <input type='text' placeholder='Name' value={name} onChange={handleChange} />
+                        <input type='number' placeholder='Age' value={age} onChange={handleChange} />
+                        <button type='button' onClick={handleClick} disabled={!name && !age}>Add</button>
+                    </div>
+                    <button type='button' disabled={!value[0]} onClick={showTable} className='preview' >
+                        Preview
+                    </button>
+                    <button type='submit' disabled={!value[0] || !isTableVisible}>Download PDF</button>
+                </form>
+            </div>
+            {isTableVisible ? <TableRowPdfData data={value} /> : ""}
+        </>
+    )
+}
+
+export default PdfDownloader;
---
+++ src/components/TableRowPdfData.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./TableRowPdfData.css";
+
+const TableRowPdfData = ({ data }) => {
+    const renderData = () => {
+        const tableData = data.map((item, index) => {
+            return (
+                <tr key={index}>
+                    <td>{item.name}</td>
+                    <td>{item.age}</td>
+                </tr>
+            )
+        })
+        return tableData;
+    }
+
+    return (
+        <div id='content' >
+            <div className='header'>Member List</div>
+            <div className='container'>
+                <table id='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderData()}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default TableRowPdfData;
